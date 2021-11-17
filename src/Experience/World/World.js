ee9e6e57cccab98746e56b9a48edb457c3beec69
Samir Ghosh <hello@samir.tech>
2021-11-16 18:30:19 -0800

    remove: unnecessary assets

---
+++ src/Experience/World/World.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three'
+import Experience from '../Experience.js'
+import Environment from './Environment.js'
+import Floor from './Floor.js'
+import Stars from './Stars.js'
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.sizes = this.experience.sizes
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        this.floor = new Floor()
+       
+        // Wait for resources
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            console.log('resources ready')
+            this.stars = new Stars()
+            this.environment = new Environment()
+        })
+    }
+    update() {
+
+        if(this.stars){
+            this.stars.update()
+        }
+    }
+}
---
+++ src/Experience/sources.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        name: 'iceColorTexture',
+        type: 'texture',
+        path: 'textures/ice/Stylized_18_Ice_basecolor.jpg'
+    },
+    {
+        name: 'iceHeightTexture',
+        type: 'texture',
+        path: 'textures/ice/Stylized_18_Ice_height.jpg'
+    },
+    {
+        name: 'iceNormalTexture',
+        type: 'texture',
+        path: 'textures/ice/Stylized_18_Ice_normal.jpg'
+    },
+    {
+        name: 'iceAmbientOcclusionTexture',
+        type: 'texture',
+        path: 'textures/ice/Stylized_18_Ice_ambientocclusion.jpg'
+    },
+    {
+        name: 'iceMetalnessTexture',
+        type: 'texture',
+        path: 'textures/ice/Stylized_18_Ice_metallic.jpg'
+    },
+    {
+        name: 'iceRoughnessTexture',
+        type: 'texture',
+        path: 'textures/ice/Stylized_18_Ice_roughness.jpg'
+    },
+    {
+        name: 'star',
+        type: 'texture',
+        path: 'textures/5.png'
+    }   
+]
