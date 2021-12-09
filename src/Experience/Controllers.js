e6297fce5f1f0e75af9385505e981af6cfacbb5e
Samir Ghosh <hello@samir.tech>
2021-12-09 13:28:44 -0800

    trying to add audio

--- src/Experience/Controllers.js
+++ src/Experience/Controllers.js
@@ -14,12 +14,17 @@ export default class Controllers
         
         
         this.controller1 = this.renderer.instance.xr.getController( 0 );
-        // this.controller1.addEventListener( 'selectstart', onSelectStart );
+        var audio = new Audio('audio/common_voice_en_10.mp3');
+        // audio.play();   
+        const onSelectStart = function(){
+            audio.play();
+        }
+        this.controller1.addEventListener( 'selectstart', onSelectStart );
         // this.controller1.addEventListener( 'selectend', onSelectEnd );
         this.scene.add( this.controller1 );
         
         this.controller2 = this.renderer.instance.xr.getController( 1 );
-        // this.controller2.addEventListener( 'selectstart', onSelectStart );
+        this.controller2.addEventListener( 'selectstart', onSelectStart );
         // this.controller2.addEventListener( 'selectend', onSelectEnd );
         this.scene.add( this.controller2 );
         
