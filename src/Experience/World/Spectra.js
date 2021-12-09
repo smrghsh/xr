ff7e1198866a007d5f1e6de5be5b303fb1b5e79e
Samir Ghosh <hello@samir.tech>
2021-12-08 22:08:30 -0800

    testing scale

--- src/Experience/World/Spectra.js
+++ src/Experience/World/Spectra.js
@@ -14,15 +14,19 @@ export default class Spectra{
         console.log(this.spectrogramImage)
         this.width = 1.564
 
-        this.waveGeometry = new THREE.PlaneGeometry(this.width,0.5)
+        this.waveGeometry = new THREE.PlaneGeometry(this.width,0.8)
 
         this.waveMaterial = new THREE.MeshBasicMaterial({map: this.waveImage, side: THREE.DoubleSide})
         this.waveMesh = new THREE.Mesh(this.waveGeometry, this.waveMaterial)
         this.waveMesh.position.y += 3
+        this.waveMesh.scale.x *= 2
+        this.waveMesh.position.z -=3
 
         this.spectrogramMaterial = new THREE.MeshBasicMaterial({map: this.spectrogramImage, side: THREE.DoubleSide})
         this.spectrogramMesh = new THREE.Mesh(this.waveGeometry, this.spectrogramMaterial)
         this.spectrogramMesh.position.y += 2
+        this.spectrogramMesh.scale.x *= 2
+        this.spectrogramMesh.position.z -=3
         this.scene.add(this.waveMesh)
         this.scene.add(this.spectrogramMesh)
     }
