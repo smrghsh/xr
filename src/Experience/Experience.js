46677f42c0b1d04cf90f6d8914324730dac22a63
a-lawn-flamingo <ywan1115@ucsc.edu>
2023-05-16 12:35:12 -0700

    add: test

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -188,7 +188,7 @@ export default class Experience
         
         //change this to be controller if controller is active
         // this.controllers.raycaster.setFromCamera( this.mouse, this.camera.instance );
-        console.log('hello')
+        console.log('hello', this.menuMesh)
         this.intersects = this.controllers.raycaster.intersectObjects( this.menuMesh, true);
         console.log(this.intersects.menuMesh.color.set('#0000ff'))
         
