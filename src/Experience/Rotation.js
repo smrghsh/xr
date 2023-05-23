a6e63cfb28f94a72c2e11732766d325f0c2bf7fb
Andrew Moshkovich <thepythonhelper@gmail.com>
2023-05-23 12:05:06 -0700

    updated distance check, cube follows exact controller pos

--- src/Experience/Rotation.js
+++ src/Experience/Rotation.js
@@ -12,10 +12,10 @@ export default class Rotation
         this.start_x;
         this.start_y;
 
-        const geometry = new THREE.BoxGeometry( .7, .7, .7 ); 
+        const geometry = new THREE.BoxGeometry( .4, .4, .4 ); 
         const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
         this.cube = new THREE.Mesh( geometry, material ); 
-        this.cube.position.z = -1;
+        this.cube.position.z = -2;
         this.cube.position.y = 1;
         this.scene.add( this.cube );
 
@@ -29,6 +29,7 @@ export default class Rotation
             this.start_x = this.controller2.position.x;
             this.start_y = this.controller2.position.y;
             this.start_z = this.controller2.position.z;
+            this.outOfRange = false;
             this.enable_rotate = true;
             console.log(this.controller2.position.distanceTo(this.cube.position));
         
@@ -40,7 +41,7 @@ export default class Rotation
     }
 
     update(){
-        if(this.enable_rotate && this.controller2.position.distanceTo(this.cube.position) < 1){
+        if(this.enable_rotate && this.controller2.position.distanceTo(this.cube.position) < 1 && this.outOfRange == false){
             /*
             let start_x_r = this.start_x - this.controller2.rotation.x
             let start_y_r = this.start_y - this.controller2.rotation.y
@@ -49,9 +50,11 @@ export default class Rotation
             this.start_x = this.controller2.rotation.x
             this.start_y = this.controller2.rotation.y
             */
-            this.cube.position.x += this.start_x - this.controller2.position.x 
-            this.cube.position.y += this.start_y - this.controller2.position.y
-            this.cube.position.z += this.start_z - this.controller2.position.z
+            this.cube.position.x = this.controller2.position.x 
+            this.cube.position.y = this.controller2.position.y
+            this.cube.position.z = this.controller2.position.z
+        } else if (this.enable_rotate && this.controller2.position.distanceTo(this.cube.position) > 1){
+            this.outOfRange = true;
         }
 
     }
