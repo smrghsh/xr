a3a0bca27ca4bf2b15fb9ab36b457fc99c42e881
Andrew Moshkovich <thepythonhelper@gmail.com>
2023-05-23 11:26:32 -0700

    added select end function

--- src/Experience/Rotation.js
+++ src/Experience/Rotation.js
@@ -8,9 +8,9 @@ export default class Rotation
         this.experience = new Experience();
         this.renderer = this.experience.renderer
         this.scene = this.experience.scene
-        let enable_rotate = false;
-        let start_x;
-        let start_y;
+        this.enable_rotate = false;
+        this.start_x;
+        this.start_y;
 
         const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
         const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
@@ -21,22 +21,26 @@ export default class Rotation
 
         this.controller2 = this.renderer.instance.xr.getController( 1 );
         this.controller2.addEventListener( 'selectstart', ( event )=> {
-            console.log("Enabling rotate: ", start_x, start_y)
-            start_x = this.controller2.rotation.x;
-            start_y = this.controller2.rotation.y;
-            enable_rotate = true;
+            console.log("Enabling rotate: ", this.start_x, this.start_y)
+            this.start_x = this.controller2.rotation.x;
+            this.start_y = this.controller2.rotation.y;
+            this.enable_rotate = true;
         
         });
+        this.controller2.addEventListener( 'selectend', ( event )=> {
+            this.enable_rotate = false;
+        });
+
     }
 
     update(){
-        if(enable_rotate){
-            start_x_r = start_x - this.controller2.rotation.x
-            start_y_r = start_y - this.controller2.rotation.y
+        if(this.enable_rotate){
+            let start_x_r = this.start_x - this.controller2.rotation.x
+            let start_y_r = this.start_y - this.controller2.rotation.y
             this.cube.rotation.x += start_x_r * .4;
             this.cube.rotation.y += start_y_r * .4;
-            start_x = this.controller2.rotation.x
-            start_y = this.controller2.rotation.y
+            this.start_x = this.controller2.rotation.x
+            this.start_y = this.controller2.rotation.y
         }
 
     }
